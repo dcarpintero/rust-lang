01647bc2d2c542de8c15355b7624214f52d0df25
Diego Carpintero <6709785+dcarpintero@users.noreply.github.com>
2023-02-24 19:24:08 +0100

    update restaurant

--- rustlings/restaurant/src/lib.rs
+++ rustlings/restaurant/src/lib.rs
@@ -44,10 +44,12 @@ mod back_of_house {
     }
 }
 
+use crate::front_of_house::hosting;
+
 pub fn eat_at_restaurant() {
     let order1 = back_of_house::Appetizer::Soup;
     let order2 = back_of_house::Appetizer::Salad;
-    
+
     // Order a breakfast in the summer with Rye toast
     let mut meal = back_of_house::Breakfast::summer("Rye");
     // Change our mind about what bread we'd like
@@ -57,4 +59,6 @@ pub fn eat_at_restaurant() {
     // The next line won't compile if we uncomment it; we're not allowed
     // to see or modify the seasonal fruit that comes with the meal
     // meal.seasonal_fruit = String::from("blueberries");
+
+    hosting::add_to_waitlist();
 }
