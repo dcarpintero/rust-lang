74ba6aedbe5e649e29a5cae9df0c05e8066bfafa
Diego Carpintero <6709785+dcarpintero@users.noreply.github.com>
2023-02-24 19:41:56 +0100

    refactor restaurant

--- rustlings/restaurant/src/lib.rs
+++ rustlings/restaurant/src/lib.rs
@@ -1,50 +1,7 @@
-mod front_of_house {
-    fn deliver_order() {}
+mod front_of_house;
+mod back_of_house;
 
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-
-        fn seat_at_table() {}
-
-        fn fix_incorrect_order() {
-            cook_order();
-            super::deliver_order();
-        }
-    
-        fn cook_order() {}
-    }
-
-    mod serving {
-        fn take_order() {}
-
-        fn serve_order() {}
-
-        fn take_payment() {}
-    }
-}
-
-mod back_of_house {
-    pub enum Appetizer {
-        Soup,
-        Salad,
-    }
-
-    pub struct Breakfast {
-        pub toast: String,
-        seasonal_fruit: String,
-    }
-
-    impl Breakfast {
-        pub fn summer(toast: &str) -> Breakfast {
-            Breakfast {
-                toast: String::from(toast),
-                seasonal_fruit: String::from("peaches"),
-            }
-        }
-    }
-}
-
-use crate::front_of_house::hosting;
+pub use crate::front_of_house::hosting;
 
 pub fn eat_at_restaurant() {
     let order1 = back_of_house::Appetizer::Soup;
