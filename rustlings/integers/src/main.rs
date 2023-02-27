727e8df0ca0c4dc0f4722b47fa9ee15adb8a6779
Diego Carpintero <6709785+dcarpintero@users.noreply.github.com>
2023-02-27 21:50:17 +0100

    refactor

--- rustlings/integers/src/main.rs
+++ rustlings/integers/src/main.rs
@@ -1,27 +1,31 @@
 use std::collections::HashMap;
 
+struct VectorMode {
+    key: Option<i32>,
+    count: u32, 
+}
+
 fn main() {
-    let v = vec![100, 32, 57, 100, 58, 65, 57, 50, 57];
-    println!("The mode of {:?} is {}", v, calculate_mode(&v));
+    let v = vec![100, 32, 57, 100, 58, 65, 57, 50, 57, 50];
+    println!("The mode of {:?} is: {}", v, calculate_mode(&v));
 }
 
 fn calculate_mode(v : &Vec<i32>) -> i32 {
     let mut hash_map = HashMap::new();
+    let mut vector_mode = VectorMode {
+        key: None,
+        count: 0,
+    };
 
     for x in v {
         let count = hash_map.entry(x).or_insert(0);
         *count += 1;
-    }
 
-    let mut key_mode = 0;
-    let mut value_mode = 0;
-
-    for (key, value) in hash_map.iter() {
-        if *value > value_mode {
-            key_mode = **key;
-            value_mode = *value;
+        if count > (&mut vector_mode.count) {
+            vector_mode.key = Some(*x);
+            vector_mode.count = *count;
         }
     }
 
-    key_mode
+    vector_mode.key.unwrap()
 }
