8b5eb11188db0e30cbbefca6cf597d9fafbd9dcf
Diego Carpintero <6709785+dcarpintero@users.noreply.github.com>
2023-02-27 23:15:46 +0100

    add Guess type

--- rustlings/guessing_game/src/main.rs
+++ rustlings/guessing_game/src/main.rs
@@ -1,5 +1,24 @@
 use rand::Rng;
-use std::{cpm::Ordering, io}
+use std::cmp::Ordering;
+use std::io;
+
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 10 {
+            panic!("Guess value must be between 1 and 10, got {}.", value);
+        }
+
+        Guess { value }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
 
 fn main() {
     println!("Guess the number between 1 and 10!");
@@ -16,14 +35,16 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = match guess.trim().parse() {
+        let guess: i32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
 
-        println!("You guessed: {guess}");
+        let guess = Guess::new(guess);
 
-        match guess.cmp(&secret_number) {
+        println!("You guessed: {}", guess.value());
+
+        match guess.value().cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
