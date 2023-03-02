631763f5b1af832f090a82b901443dc82513e9cd
Diego Carpintero <6709785+dcarpintero@users.noreply.github.com>
2023-03-02 12:04:33 +0100

    add unit tests

--- rustlings/rectangles/src/main.rs
+++ rustlings/rectangles/src/main.rs
@@ -37,3 +37,34 @@ fn main() {
         rect1.area()
     );
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 3,
+        };
+        assert!(larger.can_hold(&smaller), "Larger shall hold smaller");
+    }
+
+    #[test]
+    fn smaller_cannot_hold_longer() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 3,
+        };
+        assert!(!smaller.can_hold(&larger), "Smaller shall not hold larger");
+    }
+}
