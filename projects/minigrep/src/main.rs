07eb6bcda48e5350fa1ead97a4b7d01cffcf1787
Diego Carpintero <6709785+dcarpintero@users.noreply.github.com>
2023-03-02 14:23:05 +0100

    refactor minigrep

--- projects/minigrep/src/main.rs
+++ projects/minigrep/src/main.rs
@@ -1,10 +1,14 @@
 use std::env; // bring std::env and std::fs modules into scope
 use std::fs;
+use std::process;
 
 fn main() {
     // the collect method on an iterator turns it into a collection, such as a vector
     let args: Vec<String> = env::args().collect();
-    let config = parse_config(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file: '{}'", config.file_path);
@@ -22,10 +26,15 @@ struct Config {
     file_path: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    // In practice clone tends to be avoided to fix ownership problems because of its runtime cost
-    let query = args[1].clone();
-    let file_path = args[2].clone();
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        // In practice clone tends to be avoided to fix ownership problems because of its runtime cost
+        let query = args[1].clone();
+        let file_path = args[2].clone();
 
-    Config { query, file_path }
+        Ok(Config { query, file_path })
+    }
 }
