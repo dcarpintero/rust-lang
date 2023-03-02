d88b5daef3b3519c04af960cc3841bb5593c25fc
Diego Carpintero <6709785+dcarpintero@users.noreply.github.com>
2023-03-02 14:39:35 +0100

    refactor minigrep functions to lib

--- projects/minigrep/src/main.rs
+++ projects/minigrep/src/main.rs
@@ -1,8 +1,8 @@
 // bring modules into scope
 use std::env; 
-use std::fs;
 use std::process;
-use std::error::Error;
+
+use minigrep::Config;
 
 fn main() {
     // the collect method on an iterator turns it into a collection, such as a vector
@@ -16,37 +16,11 @@ fn main() {
     println!("In file '{}'", config.file_path);
 
     // Because run returns () in the success case, we only handle an error
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {e}");
         process::exit(1);
     }
 
     // print the vector using the debug macro
     dbg!(args);
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("\n{contents}");
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        // In practice clone tends to be avoided to fix ownership problems because of its runtime cost
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
-    }
 }
